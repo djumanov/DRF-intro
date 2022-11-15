402aa6613f04f87e8489629b21da08dff2d2301b
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-15 19:18:29 +0500

    test serialize

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+from .serializers import CompaySerailizer, ProductSerailizer
+from .models import Company, Product
 
-# Create your views here.
+@api_view(['GET'])
+def get_companies(request: Request) -> Response:
+    company = Company.objects.get(id=1)
+    data = CompaySerailizer(company).data
+    return Response({'company': data})
--- core/urls.py
+++ core/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import get_companies
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('company/', get_companies),
 ]
