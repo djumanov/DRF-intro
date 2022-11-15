2e29d0c4b2ad176381e3553ea5032374c1b327a7
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-15 19:00:04 +0500

    model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+    website = models.CharField(max_length=255)
+    logo = models.CharField(max_length=255)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    ram = models.IntegerField()
+    price = models.FloatField()
+
+    brand = models.ForeignKey(Company, on_delete=models.CASCADE)
